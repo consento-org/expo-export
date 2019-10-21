990f1014d96e1f68bf8ab1d0295b021d085681c3
Martin Heidegger <martin.heidegger@gmail.com>
2019-10-21 18:15:24 +0900

    Extracted fontNameLookup into own util and fixed hard bug when starting sketch for the first time.

--- src/expo-export.ts
+++ src/expo-export.ts
@@ -5,6 +5,7 @@ import { generateColors } from './generate/color'
 import { generateFonts } from './generate/font'
 import { generateTextStyles } from './generate/text'
 import { writeAssets } from './generate/assets'
+import { createFontNameLookup } from './util/dom'
 
 function folder (path: string): (sub: string) => string {
   const reg = /^(.*)(\.sketch)$/ig
@@ -13,31 +14,6 @@ function folder (path: string): (sub: string) => string {
   return (sub: string) => sub === '' ? base : `${base}/${sub}`
 }
 
-interface IDocumentData {
-  textStyleWithID(id: string): undefined | {
-    style(): {
-      primitiveTextStyle(): undefined | {
-        attributes(): {
-          'NSFont': {
-            fontName(): string
-          }
-        }
-      }
-    }
-  }
-}
-
-function fontNameForSet (documentData: IDocumentData, id: string): string {
-  const font = documentData.textStyleWithID(id)
-  if (font === undefined || font === null) {
-    throw new Error('Unidentified font')
-  }
-  const style = font.style().primitiveTextStyle()
-  if (style === undefined) {
-    throw new Error('This shouldnt happen, but for some reason we havnt got a reference to a style anymore, please try again')
-  }
-  return style.attributes().NSFont.fontName()
-}
 
 export default function (context: any): void {
   const document = Document.getSelectedDocument()
@@ -49,7 +25,7 @@ export default function (context: any): void {
     return alert('URL missing', 'Please save the document first!')
   }
   const target = folder(url)
-  const fontNameLookup = fontNameForSet.bind(null, context.document.documentData() as IDocumentData)
+  const fontNameLookup = createFontNameLookup(document, context.document as any)
   write(target('src/styles/Color.ts'), generateColors(document))
   write(target('src/styles/Font.ts'), generateFonts(document, fontNameLookup))
   write(target('src/styles/TextStyle.ts'), generateTextStyles(document, fontNameLookup))
--- src/util/dom.ts
+++ src/util/dom.ts
@@ -1,5 +1,37 @@
 import { Document, Layer, ILayerContainer } from 'sketch/dom'
 
+interface IDocumentData {
+  textStyleWithID(id: string): undefined | {
+    style(): {
+      primitiveTextStyle(): undefined | {
+        attributes(): {
+          'NSFont': {
+            fontName(): string
+          }
+        }
+      }
+    }
+  }
+}
+
+export function createFontNameLookup (document: Document, contextDocument: any) {
+  // Important: Font name lookup will not work for the styles unless we accessed the style property
+  document.sharedTextStyles.forEach(shared => shared.style.fontFamily)
+  
+  const documentData = contextDocument.documentData() as IDocumentData
+  return (id: string): string | undefined => {
+    const textStyle = documentData.textStyleWithID(id)
+    if (textStyle === undefined || textStyle === null) {
+      throw new Error('Unidentified font')
+    }
+    const style = textStyle.style().primitiveTextStyle()
+    if (style === undefined || style === null) {
+      throw new Error('This shouldnt happen, but for some reason we havnt got a reference to a style anymore, please try again')
+    }
+    return style.attributes().NSFont.fontName()
+  }
+}
+
 export function isLayerContainer (item: any): item is ILayerContainer {
   return item['layers'] !== undefined
 }
