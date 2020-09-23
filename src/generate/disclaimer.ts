c21aa382dbd293d8be052aa61ec39b52bb13726a
Martin Heidegger <martin.heidegger@gmail.com>
2020-09-24 01:16:06 +0900

    4.1.0

--- src/generate/disclaimer.ts
+++ src/generate/disclaimer.ts
@@ -1,1 +1,1 @@
-export const disclaimer = '// This file has been generated with expo-export@4.0.0, a Sketch plugin.'
+export const disclaimer = '// This file has been generated with expo-export@4.1.0, a Sketch plugin.'
