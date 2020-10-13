50260b6970329270b85a29d33fc9e04529092ae8
Martin Heidegger <martin.heidegger@gmail.com>
2020-10-13 23:36:25 +0900

    fix: making sure that the references of the sketch elements are forwarded properly and that the types work in strict mode without problems.

--- assets/styles/util/react/SketchElement.tsx
+++ assets/styles/util/react/SketchElement.tsx
@@ -1,8 +1,10 @@
+import React, { forwardRef, Ref } from 'react'
 import { isImageAsset, ISketchElementProps, isImagePlacement, isTextBox, isSlice9Placement, isSlice9 } from '../types'
 import { ISketchImageProps, SketchImage } from './SketchImage'
-import { ISketchTextBoxProps, SketchTextBox } from './SketchTextBox'
+import { ISketchTextBoxInputProps, ISketchTextBoxProps, ISketchTextBoxViewProps, isSketchTextBoxInputProps, SketchTextBoxInput, SketchTextBoxView } from './SketchTextBox'
 import { ISketchSlice9Props, SketchSlice9 } from './SketchSlice9'
-import { SketchPolygon } from './SketchPolygon'
+import { ISketchPolygonProps, SketchPolygon } from './SketchPolygon'
+import { Text, TextInput, View, Image } from 'react-native'
 
 function isImageProps (props: ISketchElementProps<any>): props is ISketchImageProps {
   return isImageAsset(props.src) || isImagePlacement(props.src)
@@ -16,18 +18,26 @@ function isSlice9Props (props: ISketchElementProps<any>): props is ISketchSlice9
   return isSlice9(props.src) || isSlice9Placement(props.src)
 }
 
-export function SketchElement (props: ISketchImageProps): JSX.Element
-export function SketchElement (props: ISketchTextBoxProps): JSX.Element
-export function SketchElement (props: ISketchSlice9Props): JSX.Element
-export function SketchElement (props: ISketchImageProps | ISketchTextBoxProps | ISketchSlice9Props): JSX.Element {
+export interface ISketchElement {
+  (props: ISketchImageProps & { ref?: Ref<Image> }): JSX.Element
+  (props: ISketchPolygonProps & { ref?: Ref<View> }): JSX.Element
+  (props: ISketchSlice9Props & { ref?: Ref<View> }): JSX.Element
+  (props: ISketchTextBoxViewProps & { ref?: Ref<Text> }): JSX.Element
+  (props: ISketchTextBoxInputProps & { ref?: Ref<TextInput> }): JSX.Element
+}
+
+export const SketchElement: ISketchElement = forwardRef<Text | TextInput | View | Image, ISketchImageProps | ISketchTextBoxProps | ISketchSlice9Props | ISketchPolygonProps>((props, ref): JSX.Element => {
   if (isImageProps(props)) {
-    return SketchImage(props)
+    return <SketchImage {...props} ref={ref as Ref<Image>} />
   }
   if (isTextBoxProps(props)) {
-    return SketchTextBox(props)
+    if (isSketchTextBoxInputProps(props)) {
+      return <SketchTextBoxInput {...props} ref={ref as Ref<TextInput>} />
+    }
+    return <SketchTextBoxView {...props} ref={ref as Ref<Text>} />
   }
   if (isSlice9Props(props)) {
-    return SketchSlice9(props)
+    return <SketchSlice9 {...props} ref={ref as Ref<View>} />
   }
-  return SketchPolygon(props)
-}
+  return <SketchPolygon {...props} ref={ref as Ref<View>} />
+}) as ISketchElement
--- assets/styles/util/react/SketchPolygon.tsx
+++ assets/styles/util/react/SketchPolygon.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { View, ViewStyle, ViewProps, StyleSheet } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import { ISketchElementProps, isSketchError, IPolygon } from '../types'
@@ -26,25 +26,27 @@ function getStyle (polygon: IPolygon): ViewStyle {
   return style
 }
 
-export const SketchPolygon = (props: ISketchPolygonProps): JSX.Element => {
+const styles = StyleSheet.create({
+  gradient: { width: '100%', height: '100%' }
+})
+
+export const SketchPolygon = forwardRef<View, ISketchPolygonProps>((props, ref): JSX.Element => {
+  /* eslint-disable react/prop-types */
   const polygon = props.src
   const data = polygon.fill.data
   const style = StyleSheet.compose(getStyle(polygon), props.style)
   if (!exists(data) || typeof data === 'string') {
-    return React.createElement(View, {
-      ...props,
-      style
-    })
+    return <View {...props} style={style} ref={ref} />
   }
   if (isSketchError(data)) {
     throw new Error(data.error)
   }
-  return React.createElement(LinearGradient, {
-    colors: data.gradient.stops.map(stop => stop.color),
-    locations: data.gradient.stops.map(stop => stop.position),
-    start: [data.gradient.from.x, data.gradient.from.y],
-    end: [data.gradient.to.x, data.gradient.to.y],
-    ...props,
-    style
-  })
-}
+  return <View ref={ref} style={style}><LinearGradient
+    colors={data.gradient.stops.map(stop => stop.color)}
+    locations={data.gradient.stops.map(stop => stop.position)}
+    start={[data.gradient.from.x, data.gradient.from.y]}
+    end={[data.gradient.to.x, data.gradient.to.y]}
+    {...props}
+    style={styles.gradient}
+  /></View>
+})
